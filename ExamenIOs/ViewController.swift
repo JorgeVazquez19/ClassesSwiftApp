7aff72763185ede3a4f1da85f2b4241fddd7edc2
Jorge Vazquez Requejo <jorgitoovazquez@hotmail.com>
2019-02-20 10:35:35 +0100

    Initial Commit

---
+++ ExamenIOs/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ExamenIOs
+//
+//  Created by JORGE VAZQUEZ REQUEJO on 20/2/19.
+//  Copyright © 2019 JORGE VAZQUEZ REQUEJO. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
